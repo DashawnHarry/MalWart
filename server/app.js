dc6ab1caa31ff3d44e9b7c6c30903ad20637f1d8
Brandon Jernigan <81219815+BrandonJernigan@users.noreply.github.com>
2021-11-10 14:29:38 -0700

    connected frontend test request to backend test api

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
+// const path = require("path");
 
 const express = require("express");
 const app = express();
+
+app.use(express.static("../client/dist"));
 
 const db = require("./config/db.config");
 
