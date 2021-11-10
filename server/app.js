a59570d1dd68f4c32d304723c165e515f939de6b
Brandon Jernigan <81219815+BrandonJernigan@users.noreply.github.com>
2021-11-10 11:15:12 -0700

    removed static hosting

--- server/app.js
+++ server/app.js
@@ -3,9 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-// Comment
-app.use(express.static("../client/dist"));
-
 app.listen(process.env.PORT, () => {
   console.log(`-- Listening on port ${process.env.PORT} --`);
 });
