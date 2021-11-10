9bf84418157904f614b85825b717dc4266ffec15
Brandon Jernigan <81219815+BrandonJernigan@users.noreply.github.com>
2021-11-10 12:49:26 -0700

    added simple /test route for frontend

--- server/app.js
+++ server/app.js
@@ -3,6 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.send("Working...?");
+});
+
+app.get("/api/products", (req, res) => {});
+
+app.post("/api/search", (req, res) => {});
+
 app.listen(process.env.PORT, () => {
   console.log(`-- Listening on port ${process.env.PORT} --`);
 });
