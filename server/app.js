d25b198ae612c5197f59626eb35dde359019b910
Brandon Jernigan <81219815+BrandonJernigan@users.noreply.github.com>
2021-11-12 13:27:55 -0700

    addjusted api routes for product id and name

--- server/app.js
+++ server/app.js
@@ -6,29 +6,29 @@ const app = express();
 app.use(express.static("../client/dist"));
 
 const db = require("./config/db.config");
+const sanitizer = require("sanitizer");
 
-app.get("/api/products", (req, res) => {
+app.get("/api/products", async (req, res) => {
   await db
-    .query(
-      `
-  SELECT * FROM products LIMIT 10`
-    )
+    .query(`SELECT * FROM products LIMIT 10`)
     .then((data) => res.send(data.rows))
     .catch((err) => console.log(err));
 });
 
-app.get("/api/search/:product_id", (req, res) => {
+app.get("/api/id/:product_id", async (req, res) => {
   const id = req.params.product_id;
   await db
-    .query(`SELECT * FROM producst WHERE id = $1`, id)
+    .query(`SELECT * FROM products WHERE id = $1`, [id])
     .then((data) => res.send(data.rows[0]))
     .catch((err) => console.log(err));
 });
 
-app.get("/api/search/:name", (req, res) => {
+app.get("/api/name/:name", async (req, res) => {
   const name = req.params.name.toLowerCase();
+  const nameSanitized = sanitizer.escape(name);
+
   await db
-    .query(`SELECT id, name FROM products WHERE name LIKE %$1%`, name)
+    .query(`SELECT id, name FROM products WHERE name LIKE '%${nameSanitized}%'`)
     .then((data) => res.send(data.rows))
     .catch((err) => console.log(err));
 });
