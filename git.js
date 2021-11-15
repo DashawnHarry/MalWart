9d38d8d28604dc9b75384819ff03b09089b29550
Brandon Jernigan <81219815+BrandonJernigan@users.noreply.github.com>
2021-11-15 07:27:24 -0700

    commit for continuity

--- git.js
+++ git.js
@@ -14,9 +14,45 @@ const commands = [
   "git merge main",
 ];
 
-for (let command of commands) {
-  exec(command, (err, stdout) => {
+// for (let command of commands) {
+//   exec(command, (err, stdout) => {
+//     if (err) console.log(err);
+//     console.log(stdout);
+//   });
+// }
+
+exec(
+  `git checkout main && git pull && git checkout ${branch} && git merge main`,
+  (err, stdout) => {
     if (err) console.log(err);
     console.log(stdout);
-  });
-}
+  }
+);
+
+// exec("git checkout main", (err, stdout, stderr) => {
+//   console.log("1");
+//   if (err) console.log(err);
+//   if (stderr) console.log(stderr);
+//   console.log(stdout);
+// });
+
+// exec("git pull", (err, stdout, stderr) => {
+//   console.log("2");
+//   if (err) console.log(err);
+//   if (stderr) console.log(stderr);
+//   console.log(stdout);
+// });
+
+// exec(`git checkout ${branch}`, (err, stdout, stderr) => {
+//   console.log("3");
+//   if (err) console.log(err);
+//   if (stderr) console.log(stderr);
+//   console.log(stdout);
+// });
+
+// exec("git merge main", (err, stdout, stderr) => {
+//   console.log("4");
+//   if (err) console.log(err);
+//   if (stderr) console.log(stderr);
+//   console.log(stdout);
+// });
