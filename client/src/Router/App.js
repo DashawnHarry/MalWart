37c860ab2343e1bed37d5bd48ab028fc5bf2dd9b
Brandon Jernigan <81219815+BrandonJernigan@users.noreply.github.com>
2021-11-16 11:20:15 -0700

    changed webpack to allow dynamic route

--- client/src/Router/App.js
+++ client/src/Router/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-}
-  from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../Component/Pages/app";
 import NavBar from "../Component/Components/NavBar";
 import ProductDescriptionHome from "../Component/Pages/productdesription.jsx";
@@ -12,16 +7,15 @@ import ProductDescriptionHome from "../Component/Pages/productdesription.jsx";
 export default class App extends Component {
   constructor(props) {
     super(props);
-
-
   }
   render() {
-
     return (
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='nav' element={<ProductDescriptionHome />} />
+          <Route path="/" element={<Home />} />
+          <Route path="id" element={<ProductDescriptionHome />}>
+            <Route path=":id" element={<ProductDescriptionHome />} />
+          </Route>
         </Routes>
       </Router>
     );
